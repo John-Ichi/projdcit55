f65942fb643a06e1b713b245a6f568f0f2052f03
John-Ichi <140050918+John-Ichi@users.noreply.github.com>
2025-06-16 21:32:56 +0800

    Add files via upload

--- projdcit55/user-db.php
+++ projdcit55/user-db.php
@@ -1,19 +1,158 @@
+<?php
+include '_functions.php';
+
+if (!isset($_SESSION['driver'])) {
+    header('Location: login.php');
+} else {
+    $info = getLicenseInfoUser($_SESSION['driver']);
+    $drivername = $info[0]['name'];
+    $sex = $info[0]['sex'];
+    $driveraddress = $info[0]['address'];
+    $licensenum = $info[0]['licenseNumber'];
+    $dateregistered = $info[0]['dateRegistered'];
+    $daterenewed = $info[0]['dateRenewed'] == null ? 'no record' : $info[0]['dateRenewed'];
+    $expirationdate = $info[0]['expirationDate'];
+    $status = $info[0]['status'];
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ProjectLisensya</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 
+    <header>
+
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="#">ProjectLisensya</a>
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="login.php">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="admin.php">Admin</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+
     <div class="container">
-        <form method="get" action="_functions.php">
 
-            <button name="logoutDriver" value="true">Logout</button>
+        <div class="row">
+            <div class="col">
+                <div class="row">
+                    <p><b>Name:</b> <?php echo $drivername?></p>
+                </div>
+                <div class="row">
+                    <p><b>Sex:</b> <?php echo $sex?></p>
+                </div>
+                <div class="row">
+                    <p><b>License Number:</b> <?php echo $licensenum?></p>
+                </div>
+                <div class="row">
+                    <p><b>Status:</b> <?php echo $status?></p>
+                </div>
+                <div class="row">
+                    <p><b>Date Registered:</b> <?php echo $dateregistered?></p>
+                </div>
+                <div class="row">
+                    <p><b>Date Renewed:</b> <?php echo $daterenewed?></p>
+                </div>
+                <div class="row">
+                    <p><b>Expiration Date:</b> <?php echo $expirationdate?></p>
+                </div>
+                <div class="row">
+                    <p><b>Address:</b> <?php echo $driveraddress?></p>
+                </div>
+            </div>
+            <div class="col-9">
+                <div class="row">
+                    <div class="d-flex justify-content-center">
+                        <label for="filterSelect" class="form-label">sort by resolved</label>
+                    </div>
+                    <select id="filterSelect" class="form-select">
+                        <option value="All" selected>All</option>
+                        <option value="No">Not Resolved</option>
+                        <option value="Yes">Resolved</option>
+                    </select>
+                </div>
+                <div class="row">
+                    <table class="table">
+                        <tr>
+                            <th></th>
+                            <th>Violation Committed</th>
+                            <th>Penalty</th>
+                            <th>Deadline for Settlement</th>
+                            <th>Resolved</th>
+                        </tr>
+                        <?php viewLicenseViolationsUser($licensenum)?>
+                    </table>
+                </div>
+            </div>
+        </div>
 
-        </form>
+        <div class="row">
+            <div class="d-flex justify-content-center">
+                <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#logoutModal" style="margin-top: 0.5rem;">Logout</button>
+            </div>
+        </div>
     </div>
+
+    <div class="modal" tabindex="-1" id="logoutModal">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Logout</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Do you want to logout?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <form action="_functions.php">
+                        <button type="submit" class="btn btn-danger" name="logoutDriver" value="true">Confirm</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        const filterSelect = document.getElementById('filterSelect');
+        filterSelect.addEventListener('change', function() {
+            const filterSelectValue = filterSelect.value;
+            const tableRows = document.querySelectorAll('tr');
+            tableRows.forEach(item => {
+                if (filterSelectValue == 'All') {
+                    item.style.display = 'table-row';
+                } else {
+                    if (item.querySelector('td:nth-child(5)') == null) {
+                        return null;
+                    } else {
+                        if (item.querySelector('td:nth-child(5)').innerHTML == filterSelectValue) {
+                            item.style.display = 'table-row';
+                        } else {
+                            item.style.display = 'none';
+                        }
+                    }
+                }
+            });
+        });
+    </script>
 
 </body>
 </html>
